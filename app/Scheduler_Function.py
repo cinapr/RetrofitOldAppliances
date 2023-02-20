2c5bf138c7748ddfafb168ab1c1ddaf2d3792004
cinapr <115988325+cinapr@users.noreply.github.com>
2023-02-20 15:26:12 +0200

    Fix bugs of get current time

--- app/Scheduler_Function.py
+++ app/Scheduler_Function.py
@@ -7,15 +7,21 @@ def round_dt(dt, delta):
 	return datetime.min + round((dt - datetime.min) / delta) * delta
 
 def get_schedulerTime():
-	dateSchedule = round_dt(dt, timedelta(minutes=15)) #Get the nearest time to 15 min ago
+	dt = datetime.now()
+	dateSchedule = round_dt(dt, timedelta(minutes=1)) #Get the nearest time to 15 min ago
+	#print (dateSchedule)
 	sql = SQL_Function()
-	rows = sql.select("SELECT id, User_ID, device_id, time, job, status FROM schedule WHERE time = '"+ str(dateSchedule.time()) +"'")
+	query = "SELECT id, User_ID, device_id, time, job, status FROM schedule WHERE time = '"+ str(dateSchedule.time()) +"'"
+	rows = sql.select(query)
+	#print(dateSchedule)
+	#print(query)
+	#print(rows)
 
 	countJob = 0
 	for row in rows :
-		response = Protocol_Function.control_tuya(str(row["deviceID"]), str(row["job"]), eval(str(row["status"])))
+		response = Protocol_Function.control_tuya(str(row[2]), str(row[4]), str(row[5]))
 		isPushSuccess = Protocol_Function.control_tuya_isSuccess(response)
-		Protocol_Function.push_history_tuya(SystemDeviceID, CommandType, str(isPushSuccess), "CRON")
+		Protocol_Function.push_history_tuya(str(row[2]), str(row[4]), str(isPushSuccess), "CRON")
 		countJob += 1
 	
 	del sql
